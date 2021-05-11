83b3c9672e59a995b061f87d7109d5785baaf8b6
Grant Emerson <grant.alex.emerson@gmail.com>
2021-05-11 17:13:59 -0500

    form validation almost finished

--- register.php
+++ register.php
@@ -1,95 +1,87 @@
 <?php
-function sanitizeFormPassword($inputText) {
-    $inputText = strip_tags($inputText);
-    return $inputText;
-}
+	include("includes/classes/Account.php");
+    include("includes/classes/Constants.php");
+	$account = new Account();
 
-function sanitizeFormUsername($inputText) {
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    return $inputText;
-}
-
-function sanitizeFormString($inputText) {
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    $inputText = ucfirst(strtolower($inputText));
-    return $inputText;
-}
-
-if(isset($_POST['loginButton'])) {
-    // login button was pressed
- 
-}
-if(isset($_POST['registerButton'])) {
-    // register button was pressed
-    $username = sanitizeFormUsername($_POST['username']);
-    $firstName = sanitizeFormString($_POST['firstName']);
-    $lastName = sanitizeFormString($_POST['lastName']);
-    $email = sanitizeFormString($_POST['email']);
-    $email2 = sanitizeFormString($_POST['email2']);
-    $password = sanitizeFormPassword($_POST['password']);
-    $password2 = sanitizeFormPassword($_POST['password2']);
-}
+	include("includes/handlers/register-handler.php");
+	include("includes/handlers/login-handler.php");
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Delta Raedio</title>
+	<title>Welcome to Slotify!</title>
 </head>
 <body>
-    <div id="inputContainer" class="">
-    <form action="register.php" id="loginForm" method="POST">
-    <h2>Login to your account</h2>
-    <p>
-    <label for="loginUsername">Username</label>
-    <input type="text" id="loginUsername" name="loginUsername" placeholder="Bart Simpson" required>
-    </p>
-    <p>
-    <label for="loginPassword">Password</label>
-    <input type="password" id="loginPassword" name="loginPassword" placeholder="your password" required>
-    </p>
-   <button type="submit" name="loginButton">LOG IN</button>
-    </form>
 
-    <form action="register.php" id="registerForm" method="POST">
-    <h2>Create your account</h2>
-    <p>
-    <label for="username">Username</label>
-    <input type="text" id="username" name="username" placeholder="Bart Simpson" required>
-    </p>
-    <p>
-    <label for="firstName">First Name</label>
-    <input type="text" id="firstName" name="firstName" placeholder="Bart " required>
-    </p>
-    <p>
-    <label for="lastName">Last Name</label>
-    <input type="text" id="lastName" name="lastName" placeholder="Simpson" required>
-    </p>
-    <p>
-    <label for="email">email</label>
-    <input type="email" id="email" name="email" placeholder="Bart@gmail.com" required>
-    </p>
-    <p>
-    <label for="email2">Confirm Email</label>
-    <input type="email2" id="email2" name="email2" placeholder="Bart@gmail.com" required>
-    </p>
+	<div id="inputContainer">
+		<form id="loginForm" action="register.php" method="POST">
+			<h2>Login to your account</h2>
+			<p>
+				<label for="loginUsername">Username</label>
+				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
+			</p>
+			<p>
+				<label for="loginPassword">Password</label>
+				<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="loginButton">LOG IN</button>
+			
+		</form>
 
 
-    <p>
-    <label for="password">Password</label>
-    <input type="password" id="password" name="password" placeholder="your password" required>
-    </p>
-    <p>
-    <label for="password">Confirm Password</label>
-    <input type="password2" id="password2" name="password2" required>
-    </p>
-   <button type="submit" name="registerButton">SIGN UP</button>
-    </form>
-    </div>
+
+		<form id="registerForm" action="register.php" method="POST">
+			<h2>Create your free account</h2>
+			<p>
+				<?php echo $account->getError(Constants::$usernameCharacters); ?>
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$firstNameCharacters); ?>
+				<label for="firstName">First name</label>
+				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$lastNameCharacters); ?>
+				<label for="lastName">Last name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+				<?php echo $account->getError(Constants::$emailInvalid); ?>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
+			</p>
+
+			<p>
+				<label for="email2">Confirm email</label>
+				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+				<?php echo $account->getError(Constants::$passwordDoNotAlphaNumeric); ?>
+				<?php echo $account->getError(Constants::$passwordCharacters); ?>
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" placeholder="Your password" required>
+			</p>
+
+			<p>
+				<label for="password2">Confirm password</label>
+				<input id="password2" name="password2" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="registerButton">SIGN UP</button>
+			
+		</form>
+
+
+	</div>
+
 </body>
 </html>
