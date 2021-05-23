e8b2f3997a0ddd91dae9a9663f61b791234bb10c
Grant Emerson <grant.alex.emerson@gmail.com>
2021-05-23 12:29:35 -0500

    icon images added

--- index.php
+++ index.php
@@ -20,7 +20,9 @@ else {
     
     <div id="nowPlayingBarContainer">
         <div id="nowPlayingBar">
-        
+            <div id="nowPlayingLeft"></div>
+            <div id="nowPlayingCenter"></div>
+            <div id="nowPlayingRight"></div>
         </div>
     </div>
 
