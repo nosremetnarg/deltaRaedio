b7dfcb4a6573c4fe832bee65b28899bb20ecbad4
Grant Emerson <grant.alex.emerson@gmail.com>
2021-06-10 12:36:20 -0500

    song info displays now

--- includes/classes/Album.php
+++ includes/classes/Album.php
@@ -44,7 +44,18 @@
 			return mysqli_num_rows($query);
         }
 		
-        
+        public function getSongIds() {
+			$query = mysqli_query($this->con, "SELECT id FROM Songs WHERE album='$this->id' ORDER BY 
+			albumOrder ASC");
+
+			$array = array();
+
+			while($row = mysqli_fetch_array($query)) {
+				array_push($array, $row['id']);
+			} 
+
+			return $array;
+		}
         
 	}
 ?>
