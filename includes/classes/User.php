e5999ed6bb1b7d3c92fe2176f91729d789c7ff13
Grant Emerson <grant.alex.emerson@gmail.com>
2021-08-30 09:55:53 -0500

    section 17 half done

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -13,5 +13,17 @@ class User {
     public function getUsername() {
         return $this->username;
     }
+
+    public function getEmail() {
+        $query = mysqli_query($this->con, "SELECT email FROM users WHERE USERNAME='$this->username'");
+        $row = mysqli_fetch_array($query);
+        return $row['email'];
+    }
+
+    public function getFirstAndLastName() {
+        $query = mysqli_query($this->con, "SELECT concat(firstName, ' ', lastName) as 'name' FROM users WHERE USERNAME='$this->username'");
+        $row = mysqli_fetch_array($query);
+        return $row['name'];
+    }
 }
 ?>
--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -1,37 +1,35 @@
 <div id="navBarContainer">
 	<nav class="navBar">
 
-		<span onclick="openPage('index.php')" class="logo">
+		<span role="link" tabindex="0" onclick="openPage('index.php')" class="logo">
 			<img src="assets/images/icons/logo.png">
-</span>
+		</span>
 
 
 		<div class="group">
 
 			<div class="navItem">
-				<a href="search.php" class="navItemLink">Search
+				<span role='link' tabindex='0' onclick='openPage("search.php")' class="navItemLink">
+					Search
 					<img src="assets/images/icons/search.png" class="icon" alt="Search">
-				</a>
+				</span>
 			</div>
 
 		</div>
 
 		<div class="group">
 			<div class="navItem">
-				<a href="browse.php" class="navItemLink">Browse</a>
+				<span role="link" tabindex="0" onclick="openPage('browse.php')" class="navItemLink">Browse</span>
 			</div>
 
 			<div class="navItem">
-				<a href="yourMusic.php" class="navItemLink">Your Music</a>
+				<span role="link" tabindex="0" onclick="openPage('yourMusic.php')" class="navItemLink">Your Music</span>
 			</div>
 
 			<div class="navItem">
-				<a href="profile.php" class="navItemLink">Reece Kenney</a>
+				<span role="link" tabindex="0" onclick="openPage('settings.php')" class="navItemLink"><?php echo $userLoggedIn->getFirstAndLastName(); ?></span>
 			</div>
 		</div>
-
-
-
 
 	</nav>
 </div>
